fa8300e9b0c325b1559e788eecf7e8a0109457fa
Adam Hooper <adam@patch.com>
2009-03-17 11:03:59 -0400

    Fix rdoc

--- Rakefile
+++ Rakefile
@@ -8,4 +8,5 @@ Echoe.new('html_namespacing', '0.0.1') do |p|
   p.summary = 'Automatic HTML namespacing'
   p.description = 'Inserts "class=" attributes within snippets of HTML so CSS and JavaScript can use automatic scopes'
   p.url = 'http://github.com/adamh/html_namespacing'
+  p.rdoc_files = ['README.rdoc'] # echoe seems to gather everything twice
 end
--- ext/html_namespacing/html_namespacing_ext.c
+++ ext/html_namespacing/html_namespacing_ext.c
@@ -14,11 +14,23 @@ static const HtmlNamespacingAllocationStrategy ALLOCATION_STRATEGY = {
     (void *(*)(void *, size_t)) ruby_xrealloc
 };
 
-static VALUE
+/*
+ * call-seq:
+ *   HtmlNamespacing.add_namespace_to_html(html, ns) => String
+ *
+ * Returns new HTML string based on +html+ with the  HTML class +ns+ to root
+ * elements.
+ *
+ * If +html+ is nil, returns +nil+.
+ *
+ * If +ns+ is nil, returns +html+.
+ */
+
+VALUE
 html_namespacing_add_namespace_to_html(
         VALUE obj,
-        VALUE html_value,
-        VALUE ns_value)
+        VALUE html,
+        VALUE ns)
 {
     /*
      * It's almost tempting to manually allocate the RString object to save
@@ -29,32 +41,38 @@ html_namespacing_add_namespace_to_html(
      * But for now, let's just do the extra copy (implicit in rb_str_new2) and
      * be done with it.
      */
-    const char *html;
+
+    const char *html_ptr;
     size_t html_len;
-    const char *ns;
-    char *ret;
+    const char *ns_ptr;
+    char *ret_ptr;
     size_t ret_len;
     int rv;
-    VALUE ret_value;
+    VALUE ret;
 
-    if (TYPE(html_value) == T_NIL) {
+    if (TYPE(html) == T_NIL) {
       return Qnil;
     }
-    Check_Type(html_value, T_STRING);
+    Check_Type(html, T_STRING);
 
-    if (TYPE(ns_value) == T_NIL) {
-      return html_value;
+    if (TYPE(ns) == T_NIL) {
+      return html;
     }
 
-    Check_Type(ns_value, T_STRING);
+    Check_Type(ns, T_STRING);
 
-    html = RSTRING(html_value)->ptr;
-    html_len = RSTRING(html_value)->len;
+    html_ptr = RSTRING(html)->ptr;
+    html_len = RSTRING(html)->len;
 
-    ns = RSTRING(ns_value)->ptr;
+    ns_ptr = RSTRING(ns)->ptr;
 
     rv = add_namespace_to_html_with_length_and_allocation_strategy(
-            html, html_len, ns, &ret, &ret_len, ALLOCATION_STRATEGY);
+            html_ptr,
+            html_len,
+            ns_ptr,
+            &ret_ptr,
+            &ret_len,
+            ALLOCATION_STRATEGY);
     if (rv == EINVAL) {
         rb_raise(rb_eArgError, "Badly-formed HTML");
     }
@@ -62,12 +80,15 @@ html_namespacing_add_namespace_to_html(
         rb_raise(rb_eRuntimeError, "Unknown error in add_namespace_to_html");
     }
 
-    ret_value = rb_str_new(ret, ret_len);
-    ruby_xfree(ret);
+    ret = rb_str_new(ret_ptr, ret_len);
+    ruby_xfree(ret_ptr);
 
-    return ret_value;
+    return ret;
 }
 
+/*
+ * Holds functions related to HTML namespacing.
+ */
 void
 Init_html_namespacing_ext()
 {
