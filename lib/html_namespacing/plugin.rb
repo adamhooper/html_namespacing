2b8d752d89f8d50c3b4117a3bbceaa7420265a33
adamh <adam@adamhooper.com>
2009-07-28 15:38:09 -0400

    Add JavaScript namespacing

--- lib/html_namespacing/plugin.rb
+++ lib/html_namespacing/plugin.rb
@@ -4,9 +4,7 @@ module HtmlNamespacing
     autoload(:Sass, File.dirname(__FILE__) + '/plugin/sass')
 
     def self.default_relative_path_to_namespace(path)
-      if path =~ /^([^\.]+)/
-        $1.gsub(/\//, '-')
-      end
+      path.gsub(/\//, '-')
     end
   end
 end
--- lib/html_namespacing/plugin/rails.rb
+++ lib/html_namespacing/plugin/rails.rb
@@ -3,15 +3,15 @@ module HtmlNamespacing
     module Rails
       def self.install(options = {})
         @options = options
-        install_rails_2_3
+        install_rails_2_3(options)
       end
 
       # Called by ActionView
-      def self.path_to_namespace(template)
-        if func = @options[:template_to_namespace_callback]
-          func.call(template)
+      def self.path_to_namespace(relative_path)
+        if func = @options[:path_to_namespace_callback]
+          func.call(relative_path)
         else
-          HtmlNamespacing::Plugin.default_relative_path_to_namespace(template.path)
+          HtmlNamespacing::Plugin.default_relative_path_to_namespace(relative_path)
         end
       end
 
@@ -23,40 +23,111 @@ module HtmlNamespacing
         end
       end
 
-      private
+      def self.javascript_root
+        @options[:javascript_root] || RAILS_ROOT + '/app/javascripts/views'
+      end
 
-      def self.install_rails_2_3(options = {})
-        ::ActionView::Template.class_eval do
-          def render_template_with_html_namespacing(*args)
-            s = render_template_without_html_namespacing(*args)
-            namespace_rendered_content(s)
+      def self.javascript_optional_suffix
+        @options[:javascript_optional_suffix]
+      end
+
+      module Helpers
+        def html_namespacing_javascript_tag(framework)
+          files = html_namespacing_files
+          unless files.empty?
+            r = "<script type=\"text/javascript\"><!--//--><![CDATA[//><!--\n"
+            r << HtmlNamespaceJs[framework][:top]
+            r << "\n"
+            files.collect do |relative_path, absolute_path|
+              r << html_namespacing_inline_js(framework, relative_path, absolute_path)
+              r << "\n"
+            end
+            r << "//--><!]]></script>"
           end
-          alias_method_chain :render_template, :html_namespacing
+        end
 
-          def render_with_html_namespacing(view, local_assigns={})
-            s = render_without_html_namespacing(view, local_assigns)
-            namespace_rendered_content(s)
-          end
-          alias_method_chain :render, :html_namespacing
+        private
 
-          def namespace_rendered_content(content)
-            if format == 'html'
-              begin
-                HtmlNamespacing::add_namespace_to_html(content, html_namespace)
-              rescue ArgumentError => e
-                view = args.first
-                HtmlNamespacing::Plugin::Rails.handle_exception(e, self, view)
-                content # unless handle_exception() raised something
-              end
+        HtmlNamespaceJs = {
+          :jquery => {
+            :top => File.open(File.join(File.dirname(__FILE__), 'dom_scan_jquery.compressed.js')) { |f| f.read },
+            :each => 'jQuery(function($){var NS=%s,$NS=function(){return $($.NS[NS])};%s});'
+          }
+        }
+
+        def html_namespacing_files
+          suffix = ::HtmlNamespacing::Plugin::Rails.javascript_optional_suffix
+          root = ::HtmlNamespacing::Plugin::Rails.javascript_root
+
+          r = html_namespacing_rendered_paths
+          
+          r.uniq!
+
+          r.collect! do |relative_path|
+            prefix = File.join(root, relative_path)
+            absolute_path = "#{prefix}.js"
+            absolute_path_with_suffix = "#{prefix}.#{suffix}.js" if suffix
+
+            if suffix && File.exist?(absolute_path_with_suffix)
+              [relative_path, absolute_path_with_suffix]
+            elsif File.exist?(absolute_path)
+              [relative_path, absolute_path]
             else
-              content
+              nil
             end
           end
 
-          def html_namespace
-            HtmlNamespacing::Plugin::Rails.path_to_namespace(self)
+          r.compact!
+
+          r
+        end
+
+        def html_namespacing_inline_js(framework, relative_path, absolute_path)
+          namespace = ::HtmlNamespacing::Plugin::Rails.path_to_namespace(relative_path)
+          content = File.open(absolute_path) { |f| f.read }
+
+          HtmlNamespaceJs[framework][:each] % [namespace.to_json, content]
+        end
+      end
+
+      private
+
+      def self.install_rails_2_3(options = {})
+        if options[:javascript]
+          ::ActionView::Base.class_eval do
+            attr_writer(:html_namespacing_rendered_paths)
+            def html_namespacing_rendered_paths
+              @html_namespacing_rendered_paths ||= []
+            end
           end
-          memoize :html_namespace
+        end
+
+        ::ActionView::Template.class_eval do
+          def render_with_html_namespacing(view, local_assigns = {})
+            html = render_without_html_namespacing(view, local_assigns)
+
+            view.html_namespacing_rendered_paths << path_without_format_and_extension if view.respond_to?(:html_namespacing_rendered_paths)
+
+            if format == 'html'
+              add_namespace_to_html(html, view)
+            else
+              html
+            end
+          end
+          alias_method_chain :render, :html_namespacing
+
+          private
+
+          def add_namespace_to_html(html, view)
+            HtmlNamespacing::add_namespace_to_html(html, html_namespace)
+          rescue ArgumentError => e
+            HtmlNamespacing::Plugin::Rails.handle_exception(e, self, view)
+            html # unless handle_exception() raised something
+          end
+
+          def html_namespace
+            HtmlNamespacing::Plugin::Rails.path_to_namespace(path_without_format_and_extension)
+          end
         end
       end
     end
